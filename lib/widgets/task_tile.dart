efd6ae6dfb518fcad1ddd7f0561aaa43d5ffaaf8
denyanwardev <denyanwardev@gmail.com>
2023-06-09 16:26:04 +0800

    setting state task_tile

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -9,10 +9,29 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
-      ),
+      trailing: TaskCheckBox(),
+    );
+  }
+}
+
+class TaskCheckBox extends StatefulWidget {
+  @override
+  State<TaskCheckBox> createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: (bool? newValue) {
+        setState(() {
+          isChecked = newValue!;
+        });
+      },
     );
   }
 }
